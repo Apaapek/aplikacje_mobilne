ab7ee0cfe8645cb45e2a51fb28859dcbacf6a7b4
Apaap <maciekjeremi@wp.pl>
2023-11-21 10:18:47 +0100

    Zadanie 6 fix

---
+++ Zadanie_6/zadanie6/zadanie6/zadanie6/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  zadanie6
+//
+//  Created by student on 21/11/2023.
+//  Copyright © 2023 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIButton *informationButton;
+@property (nonatomic,weak) IBOutlet UIImageView *UIImageView;
+-(IBAction)enter;
+
+@end
+
