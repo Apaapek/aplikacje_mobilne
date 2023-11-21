2de1ed74501f08eb6e42dd892ffd29a61ca138c4
Apaap <maciekjeremi@wp.pl>
2023-11-21 08:54:14 +0100

    Zadanie 6

---
+++ Zadanie_6/zadanie6/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zadanie6
+//
+//  Created by student on 14/11/2023.
+//  Copyright © 2023 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
